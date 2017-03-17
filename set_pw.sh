723029fe74dcc3faacdc92dd0aabce1f10160228
Remmelt Pit <rpit@ebay.com>
2017-03-17 17:45:06 +0100

    First commit

---
+++ set_pw.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+if [ $# -ne 1 ]; then
+    echo "Please provide the image name"
+    exit 1
+fi
+
+img="/images/${1}"
+
+echo "HI! I'm going to set SSH access and wifi config."
+echo
+echo -n "Trying to find ${img} folder... "
+if [ -f "${img}" ]; then
+    echo found!
+else
+    echo
+    echo Could not find the image file...
+    exit 2
+fi
+echo
+
+mkdir -p /mnt/disk1 /mnt/disk2
+
+for nr in 1 2; do
+    i=$(fdisk -l "${img}" | grep "${img}${nr}" | awk '{print "offset=" $2*512 ",sizelimit=" $3*512}')
+    mount -o loop,${i} "${img}" /mnt/disk${nr}
+done
+
+# Enable ssh access by putting a file named 'ssh' in the boot partition
+touch $(dirname $(find /mnt -name kernel.img))/ssh
+
+echo -n "Your SSID: "
+read ssid
+echo -n "Your wifi password: "
+read pw
+
+# Find the wpa_supplicant file
+wpa_supplicant=$(find /mnt -name wpa_supplicant.conf | grep /etc/wpa_supplicant/wpa_supplicant.conf)
+
+cat << EOF > ${wpa_supplicant}
+network={
+ssid="${ssid}"
+psk="${pw}"
+}
+EOF
+
+umount /mnt/disk1 /mnt/disk2
+
+echo
+echo "We're done!"
+echo
